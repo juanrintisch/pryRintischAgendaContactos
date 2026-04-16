18232af61bfdff918ca988b980fe47f9b2dd1bec
juanrintisch <juanrintisch@gmail.com>
2026-04-16 13:41:33 -0300

    Terminado

--- pryRintischAgendaContactos/frmUno.cs
+++ pryRintischAgendaContactos/frmUno.cs
@@ -30,36 +30,46 @@ namespace pryRintischAgendaContactos
         {
             if (txtNombre.Text == "")
             {
+                
                 MessageBox.Show("Debe Ingresar un Nombre", "Upss!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 txtNombre.Focus();
+
             }
 
             else if (txtApellido.Text == "")
             {
+                
                 MessageBox.Show("Debe Ingresar un Apellido", "Upss!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 txtApellido.Focus();
+
             }
 
             else if (mskNumero.Text == "(351)   -")
             {
+               
                 MessageBox.Show("Debe Ingresar un Numero de Telefono", "Upss!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 mskNumero.Focus();
+
             }
 
             else if (txtCorreo.Text == "")
             {
+                
                 MessageBox.Show("Debe Ingresar un Correo Electronico", "Upss!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 txtCorreo.Focus();
+
             }
 
             else if (cmbCategoria.Text == "")
             {
+               
                 MessageBox.Show("Debe Seleccionar una Categoria", "Upss!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 cmbCategoria.Focus();
 
             }
             else if (txtNombre.Text != "" && txtApellido.Text != "" && mskNumero.Text != "(351)   -" && txtCorreo.Text != "" && cmbCategoria.Text != "")
             {
+                
                 dtaDatos.Rows.Add(txtNombre.Text, txtApellido.Text, mskNumero.Text, txtCorreo.Text, cmbCategoria.Text);
                 txtNombre.Clear();
                 txtApellido.Clear();
@@ -68,9 +78,12 @@ namespace pryRintischAgendaContactos
                 cmbCategoria.SelectedIndex = -1;
                 txtNombre.Focus();
                 MessageBox.Show("Contacto Agregado Correctamente", "Contactos", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }    
+
+            } 
+            
             brnEditar.Enabled = true;
             btnEliminar.Enabled = true;
+
         }
                               
         private void txtNombre_TextChanged(object sender, EventArgs e)
@@ -80,20 +93,26 @@ namespace pryRintischAgendaContactos
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
+          
            var Resultado = MessageBox.Show("¿Desea Eliminar el Contacto?", "Contactos", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
            if (Resultado == DialogResult.Yes)
            {
-               dtaDatos.Rows.RemoveAt(dtaDatos.CurrentRow.Index);
+               
+                dtaDatos.Rows.RemoveAt(dtaDatos.CurrentRow.Index);
                MessageBox.Show("Contacto Eliminado Correctamente", "Contactos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
            }
             else
             {
+                
                 MessageBox.Show("No se Elimino el Contacto", "Contactos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             }
         }
 
         private void brnEditar_Click(object sender, EventArgs e)
         {
+            
             txtNombre.Text = dtaDatos.CurrentRow.Cells[0].Value.ToString();
             txtApellido.Text = dtaDatos.CurrentRow.Cells[1].Value.ToString();
             mskNumero.Text = dtaDatos.CurrentRow.Cells[2].Value.ToString();
@@ -101,6 +120,7 @@ namespace pryRintischAgendaContactos
             cmbCategoria.Text = dtaDatos.CurrentRow.Cells[4].Value.ToString();
             btnConfirmarEdicion.Enabled = true;
             txtNombre.Focus();
+
         }
 
         private void dtaDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -115,25 +135,32 @@ namespace pryRintischAgendaContactos
 
         private void btnConfirmarEdicion_Click(object sender, EventArgs e)
         {           
+           
            var Resultado = MessageBox.Show("¿Guardar cambios?", "Contactos", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
            if (Resultado == DialogResult.Yes)
            {
+               
                dtaDatos.CurrentRow.Cells[0].Value = txtNombre.Text;
                dtaDatos.CurrentRow.Cells[1].Value = txtApellido.Text;
                dtaDatos.CurrentRow.Cells[2].Value = mskNumero.Text;
                dtaDatos.CurrentRow.Cells[3].Value = txtCorreo.Text;
                dtaDatos.CurrentRow.Cells[4].Value = cmbCategoria.Text;
+
                MessageBox.Show("Contacto Editado Correctamente", "Contactos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
                txtNombre.Clear();
                txtApellido.Clear();
                mskNumero.Clear();
                txtCorreo.Clear();
                cmbCategoria.SelectedIndex = -1;
                txtNombre.Focus();
+
            }
             else
             {
+               
                 MessageBox.Show("No se Realizaron cambios", "Contactos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             }
         }
 
